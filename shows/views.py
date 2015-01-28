ced9fb5fcdd105a67dafcc9f8531b9c84c6827ed
Jonathan Hernandez <jbhernan@gmail.com>
2015-01-27 20:23:10 -0800

    adding basic files - copied from weatherproj

---
+++ shows/views.py
@@ -0,0 +1,18 @@
+from django import http
+from django.template import RequestContext, loader
+import urllib2
+import json
+import sys
+from bs4 import BeautifulSoup
+from datetime import date
+import json
+from json import JSONEncoder
+from django.core.serializers.json import DjangoJSONEncoder
+from django.utils.safestring import mark_safe
+from sets import Set
+
+def home(request):
+  return http.HttpResponse(template.render(context))
+
+
+
