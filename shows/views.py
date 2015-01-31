21e81c11e1e78d62847bda052ec326cf2e839349
Jonathan Hernandez <jbhernan@gmail.com>
2015-01-31 12:14:04 -0800

    work on view

--- shows/views.py
+++ shows/views.py
@@ -10,9 +10,15 @@ from json import JSONEncoder
 from django.core.serializers.json import DjangoJSONEncoder
 from django.utils.safestring import mark_safe
 from sets import Set
-
+from shows.models import Episode
 def home(request):
-  return http.HttpResponse("Shows home page")
+  episodes = Episode.objects().all()
+  episodes = sorted(episodes, key=lambda sn: sn.show_name)
+  outStr = "Shows home page";
+  for episode in episodes:
+    outStr += episode.show_name + ": " + episode.episode_date + "  " + episode.episode_name;
+    outStr += "<br/>\n";
+  return http.HttpResponse(outStr);
 
 
 
