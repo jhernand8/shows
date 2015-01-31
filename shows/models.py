679466c92752d5f359771367b72e92bc962b3137
Jonathan Hernandez <jbhernan@gmail.com>
2015-01-31 12:08:29 -0800

    added model

--- shows/models.py
+++ shows/models.py
@@ -1,2 +1,8 @@
 from django.db import models
 
+# Object for an episode of a show - the show name, wikipedia url, episode name, and date.
+class Episode(models.Model):
+  show_name = models.TextField()
+  wiki_url = models.TextField()
+  episode_name = models.TextField()
+  episode_date = models.TextField()
