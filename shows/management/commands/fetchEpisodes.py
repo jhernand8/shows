a342aa4dafbc7c0ca1958dba3cc2808ffd44bc1d
Jonathan Hernandez <jbhernan@gmail.com>
2015-09-13 10:39:21 -0700

    look for new seasons

--- shows/management/commands/fetchEpisodes.py
+++ shows/management/commands/fetchEpisodes.py
@@ -25,7 +25,31 @@ class Command(BaseCommand):
       # handle current show
       url = show.wiki_url
       self.update_episodes_for_show(url, show.show_name)
-    
+ 
+   # check for new seasons
+   self.check_new_seasons();
+      
+  # Checks for any new seasons of the show and if so adds them to the databae  
+  def check_new_seasons(self):
+    shows = Show.objects.all()
+    for show in shows:
+      resp = urllib2.urlopen(show.wiki_url)
+      html = BeautifulSoup(resp.read())
+      try:
+        nextSeason = html.findAll(text="Next").[0].parent.find_next_sibling("a").get('href');
+        nsurl = 'http://en.wikipedia.org/' + nextSeason;
+        name = html.findAll(text="Next").[0].parent.find_next_sibling("a").contents[0];
+        season_exists = False
+        for s in shows:
+          if s.wiki_url == nsurl:
+            season_exists = True
+        if season_exists:
+          continue
+        newSeason = Show(show_name = (show.show_name + name), wiki_url = nsurl);
+        newSeason.save()
+      except:
+        continue;
+ 
   def update_episodes_for_show(self, url, showname):
     response = urllib2.urlopen(url)
     data = response.read()
