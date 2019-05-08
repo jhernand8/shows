4513b8ab4321591e34571d6e6489c0f144c0d183
Jonathan Hernandez <jbhernan@gmail.com>
2019-05-07 22:55:04 -0700

    urls

--- shows/urls.py
+++ shows/urls.py
@@ -1,12 +1,16 @@
-from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.urls import path
 
-urlpatterns = patterns('',
+admin.autodiscover()
+
+import shows.views
+
+urlpatterns = [
     # Examples:
     # url(r'^$', 'shows.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^home/', 'shows.views.home', name='home'),
-    url(r'^/', 'shows.views.home', name='home'),
-)
+    path('admin/', admin.site.urls),
+    path('home/', shows.views.home),
+    path('/', shows.views.home),
+]
