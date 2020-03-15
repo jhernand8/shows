45caa1a3611463b86c178c2672132603ffb08027
Jonathan Hernandez <jbhernan@gmail.com>
2020-03-14 18:34:34 -0700

    small fix to weekday

--- shows/views.py
+++ shows/views.py
@@ -51,19 +51,19 @@ def home(request):
 
 # Returns day string for day of week.
 def form_weekday_str(weekday):
-  if weekday == 0:
-    return "Sunday"
-  if weekday == 1:
-    return "Monday"
-  if weekday == 2:
-    return "Tuesday"
-  if weekday == 3:
-    return "Wednesday"
-  if weekday == 4:
-    return "Thursday"
-  if weekday == 5:
-    return "Friday"
   if weekday == 6:
+    return "Sunday"
+  if weekday == 0:
+    return "Monday"
+  if weekday == 1:
+    return "Tuesday"
+  if weekday == 2:
+    return "Wednesday"
+  if weekday == 3:
+    return "Thursday"
+  if weekday == 4:
+    return "Friday"
+  if weekday == 5:
     return "Saturday"
   return ""
 
